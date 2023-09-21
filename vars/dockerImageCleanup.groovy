271491fb5c5252ff0be1a6d039990e624f2e9986
Atin Mondal <atincse0@gmail.com>
2023-09-22 02:07:20 +0530

    changed docker image scan and push groovy for ecr

--- vars/dockerImageCleanup.groovy
+++ vars/dockerImageCleanup.groovy
@@ -1,6 +1,14 @@
-def call(String projectName,String imageTag, String dockerHubUser){
+// def call(String projectName,String imageTag, String dockerHubUser){
+//     sh """
+//         docker rmi ${dockerHubUser}/${projectName}:${imageTag}
+//         docker rmi ${dockerHubUser}/${projectName}:latest
+//     """ 
+// }
+
+// Docker image cleanup for ECR
+
+def call(String aws_account_id,String region, String ecrRepositoryName){
     sh """
-        docker rmi ${dockerHubUser}/${projectName}:${imageTag}
-        docker rmi ${dockerHubUser}/${projectName}:latest
+        docker rmi ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecrRepositoryName}:latest
     """ 
 }
