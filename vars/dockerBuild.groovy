4061fc20dde3014e8d17143bfc7aa8db47a0b95b
Atin Mondal <atincse0@gmail.com>
2023-09-22 01:18:28 +0530

    changed build groovy for ecr

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,15 @@
-def call(String projectName,String imageTag, String dockerHubUser){
+// def call(String projectName,String imageTag, String dockerHubUser){
+//     sh """
+//         docker image build -t ${dockerHubUser}/${projectName} .
+//         docker image tag ${dockerHubUser}/${projectName} ${dockerHubUser}/${projectName}:${imageTag}
+//         docker image tag ${dockerHubUser}/${projectName} ${dockerHubUser}/${projectName}:latest
+//     """ 
+// }
+
+// Docker Build for ECR
+def call(String aws_account_id,String region, String ecrRepositoryName){
     sh """
-        docker image build -t ${dockerHubUser}/${projectName} .
-        docker image tag ${dockerHubUser}/${projectName} ${dockerHubUser}/${projectName}:${imageTag}
-        docker image tag ${dockerHubUser}/${projectName} ${dockerHubUser}/${projectName}:latest
-    """ 
+        docker build -t ${ecrRepositoryName} .
+        docker tag ${ecrRepositoryName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecrRepositoryName}:latest
+    """
 }
